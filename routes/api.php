d79a8aaa56d28282f874d2b0c406c1e834c25a48
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-18 23:53:57 +0530

    made changes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/create_user');
+
+/*
+Route::post(`/create_user`, function
+(){
+   return ['message' => 'hello'];
+});
+Route::post(`/delete_user`, function
+(){
+    return ['message' => 'hello'];
+});
+Route::post(`/fetch_users_by_id`, function
+(){
+    return ['message' => 'hello'];
+});
+Route::post(`/fetch_all_users`, function
+(){
+    return ['message' => 'hello'];
+});*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::resource('posts', PostsController::class);
+
+Route::get('/user', [UsersController::class, 'getAllUsers']);
+Route::get('/user/{id}', [UsersController::class, 'getUserByID']);
+Route::post('/user', [UsersController::class, 'createUserByID']);
+Route::delete('/user/{id}', [UsersController::class, 'delete']);
